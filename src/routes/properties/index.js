c21e5819de8cc3c7c9220aebbce5c5595234fe6d
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-02-11 20:29:28 +0600

    add properties code added

--- src/routes/properties/index.js
+++ src/routes/properties/index.js
@@ -1,7 +1,9 @@
 // this is api rout for properties authectication
 const propertiesFindAll = require('../../api/properties/properties');
+const propertiesInsert = require('../../api/properties/propertiesInset');
 const router = require('express').Router();
 
+router.post("/properties", propertiesInsert)
 router.get("/properties", propertiesFindAll);
 
 module.exports = router
